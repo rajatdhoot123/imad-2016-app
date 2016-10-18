8a1f035d20011bc7ea7ecb391908679f6fbc450d
rajatdhoot123 <rajatdhoot123@gmail.com>
2016-10-18 19:48:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/profil.html', function (req, res) {
+app.get('/profil.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'profil.html'));
 });
 
