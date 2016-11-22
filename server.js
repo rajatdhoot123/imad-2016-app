c02d1ee3faaec96d78f3ba8b889e59843820354d
rajatdhoot123 <rajatdhoot123@gmail.com>
2016-11-22 13:31:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,19 +13,23 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/styleprofile.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'styleprofile.css'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/facebook.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'facebook.png'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/twitter.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'twitter.png'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/googleplus.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'googleplus.png'));
 });
 
